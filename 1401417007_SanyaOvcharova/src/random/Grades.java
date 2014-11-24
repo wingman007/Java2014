55e1cce6c58b3b72ec05ad652673ce840b207488
SanyaNo <sanya.no@abv.bg>
2014-11-24 23:27:29 +0200

    Another

--- 1401417007_SanyaOvcharova/src/random/Grades.java
+++ 1401417007_SanyaOvcharova/src/random/Grades.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 import java.io.IOException;
-
+/**
+ * 
+ * @author SanyaNo
+ *
+ */
 public class Grades {
 
 	static Scanner myScanner = new Scanner(System.in);
@@ -102,6 +106,13 @@ public class Grades {
 		return result;
 
 	}
+	
+	/**
+	 * Exit the programme
+	 * 
+	 * @return boolean
+	 * @throws IOException
+	 */
 
 	public boolean partyBreaker() throws IOException {
 		boolean eval = true;
