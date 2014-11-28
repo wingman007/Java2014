8d9448dacf116c08dd4fe34b86dcc25e63beb285
SanyaNo <sanya@abv.bg>
2014-11-28 19:17:18 +0200

    ex4 fix Ex_10 //better function for conversion to binary added

--- 1401417007_SanyaOvcharova/fixedApp/src/ex3/Ex3_10.java
+++ 1401417007_SanyaOvcharova/fixedApp/src/ex3/Ex3_10.java
@@ -2,83 +2,102 @@ package ex3;
 
 class Ex3_10 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-		/**
-		 * Да се създаде програма за преобразуване на произволно цяло десетично
-		 * число въведено от клавиатурата в двоично. Да се отпечата на конзолата
-		 * двоичното число.
-		 */
+        /**
+         * Да се създаде програма за преобразуване на произволно цяло десетично
+         * число въведено от клавиатурата в двоично. Да се отпечата на конзолата
+         * двоичното число.
+         */
+        System.out.println(conToBi(39));
+        conToBi2(39);
+        conToBi3(39);
+        // conToBi2(7);
+        conToDec(100111);
+        switch (3) {
 
-		System.out.println(conToBi(39));
-		conToBi2(39);
-		// conToBi2(7);
-		conToDec(100111);
-		switch (3) {
+            case 1:
+                System.out.println("Takos");
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
 
-			case 1:
-				System.out.println("Takos");
-				break;
-			case 2:
-				break;
-			case 3:
-				break;
+        }
 
-		}
+    }
 
-	}
+    public static String conToBi(int num) {
 
-	public static String conToBi(int num) {
+        String numStr = Integer.toBinaryString(num);
 
-		String numStr = Integer.toBinaryString(num);
+        return numStr;
+    }
 
-		return numStr;
-	}
+    public static void conToBi2(int num) {
+        int counter = 0;
+        int[] digits = new int[8];
+        while (true) {
 
-	public static void conToBi2(int num) {
-		int counter = 0;
-		int[] digits = new int[8];
-		while (true) {
+            int digit = num % 2;
+            num = (int) num / 2;
 
-			int digit = num % 2;
-			num = (int) num / 2;
-			
-			digits[counter] = digit;
-			counter++;
-			
+            digits[counter] = digit;
+            counter++;
 
-			if (num <= 0)
-				break;
-		}
-		
-		for (int i = (digits.length-1); i>=0; i--){
-			System.out.print(digits[i]);
-		}
-		System.out.println();
 
-	}
+            if (num <= 0) {
+                break;
+            }
+        }
 
-	public static void conToDec(int bi) {
+        for (int i = (digits.length - 1); i >= 0; i--) {
+            System.out.print(digits[i]);
+        }
+        System.out.println();
 
-		String bin = "" + bi;
-		int sum = 0;
-		int length = bin.length() - 1;
+    }
 
-		for (int i = 0; i <= (bin.length() - 1); i++) {
+    public static void conToBi3(int num) {
+        String digits = "";
+        while (true) {
 
-			int result = Integer.parseInt("" + bin.charAt(i));
+            int digit = num % 2;
+            num = (int) num / 2;
 
-			sum += result * Math.pow(2, length);
+            digits = digit + digits;
 
-			length--;
 
-		}
+            if (num <= 0) {
+                break;
+            }
+        }
 
-		System.out.println(sum);
+        System.out.println(digits);
 
-		// an*p^n + a(n-1)*p^n-1
+    }
 
-	}
+    public static void conToDec(int bi) {
 
+        String bin = "" + bi;
+        int sum = 0;
+        int length = bin.length() - 1;
+
+        for (int i = 0; i <= (bin.length() - 1); i++) {
+
+            int result = Integer.parseInt("" + bin.charAt(i));
+
+            sum += result * Math.pow(2, length);
+
+            length--;
+
+        }
+
+        System.out.println(sum);
+
+        // an*p^n + a(n-1)*p^n-1
+
+    }
 }
