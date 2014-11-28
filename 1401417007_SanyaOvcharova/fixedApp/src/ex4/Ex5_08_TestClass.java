3f71f0c1e97e5e61ff3baec30427dfbdc7e732fd
SanyaNo <sanya@abv.bg>
2014-11-28 22:27:44 +0200

    Ex5 testing

---
+++ 1401417007_SanyaOvcharova/fixedApp/src/ex4/Ex5_08_TestClass.java
@@ -0,0 +1,17 @@
+package ex4;
+
+/**
+ *
+ * @author SanyaNo
+ */
+public class Ex5_08_TestClass {
+
+    String greetings = "Greetings from package ex4";
+
+    public static void main(String[] args) {
+    }
+
+    public Ex5_08_TestClass() {
+        System.out.println(greetings);
+    }
+}
