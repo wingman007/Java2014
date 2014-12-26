4035f49fa1c5ba2967556e11f7268ad827ab82a9
FEarBG <fearbg90@gmail.com>
2014-12-26 14:34:23 +0200

    :boom: Update

--- 1401417010_Pavlin/01. Laboratory/Кирил Иванов/2013/Exercise_1_1/src/exercise_1_1/ChessBoard.java
+++ 1401417010_Pavlin/01. Laboratory/Кирил Иванов/2013/Exercise_1_1/src/exercise_1_1/ChessBoard.java
@@ -5,66 +5,23 @@ public class ChessBoard {
     protected static void printBoard(ChessPosition figure1, ChessPosition figure2) {
         
         String[][] chessArray = new String[8][8];
-
-        int number = 8;
         
-        for(int i = 0; i < 8; i++) {
-
-            chessArray[i][0] = "a".concat(Integer.toString(number));
+        int number = chessArray.length;
+        
+        for (String[] elements : chessArray) {
+            
+            elements[0] = "a".concat(Integer.toString(number));
+            elements[1] = "b".concat(Integer.toString(number));
+            elements[2] = "c".concat(Integer.toString(number));
+            elements[3] = "d".concat(Integer.toString(number));
+            elements[4] = "e".concat(Integer.toString(number));
+            elements[5] = "f".concat(Integer.toString(number));
+            elements[6] = "g".concat(Integer.toString(number));
+            elements[7] = "h".concat(Integer.toString(number));
+            
             number--;
         }
-        
-        number = 8;
-        
-        for(int i = 0; i < 8; i++) {
 
-            chessArray[i][1] = "b".concat(Integer.toString(number));
-            number--;
-        }
-        
-        number = 8;
-        
-        for(int i = 0; i < 8; i++) {
-
-            chessArray[i][2] = "c".concat(Integer.toString(number));
-            number--;
-        }
-        number = 8;
-        
-        for(int i = 0; i < 8; i++) {
-
-            chessArray[i][3] = "d".concat(Integer.toString(number));
-            number--;
-        }
-        number = 8;
-        
-        for(int i = 0; i < 8; i++) {
-
-            chessArray[i][4] = "e".concat(Integer.toString(number));
-            number--;
-        }
-        number = 8;
-        
-        for(int i = 0; i < 8; i++) {
-
-            chessArray[i][5] = "f".concat(Integer.toString(number));
-            number--;
-        }
-        number = 8;
-        
-        for(int i = 0; i < 8; i++) {
-
-            chessArray[i][6] = "g".concat(Integer.toString(number));
-            number--;
-        }
-        number = 8;
-        
-        for(int i = 0; i < 8; i++) {
-
-            chessArray[i][7] = "h".concat(Integer.toString(number));
-            number--;
-        }
-        
         System.out.println("\n-----------------------------------------");
         
         for(String[] rows : chessArray) {
